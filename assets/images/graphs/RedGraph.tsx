4e15c8ea1cf53e9e8de37595d1b2bc25e633a3f2
DmitriyGrosh <dmitriy.grosh@gmail.com>
2021-10-24 11:22:43 +0300

    fix svgs

--- assets/images/graphs/RedGraph.tsx
+++ assets/images/graphs/RedGraph.tsx
@@ -1,21 +1,17 @@
 import React from 'react';
 
 const RedGraph = () => (
-	<svg width="68" height="21" viewBox="0 0 68 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-		<rect opacity="0.7" width="17" height="21" rx="1" fill="#05050F" />
-		<rect opacity="0.7" x="19" width="14" height="21" rx="1" fill="#05050F" />
-		<rect opacity="0.7" x="35" width="14" height="21" rx="1" fill="#05050F" />
-		<rect opacity="0.7" x="51" width="17" height="21" rx="1" fill="#05050F" />
-		<path
-			d="M0 6C8.62687 6 5.35075 12.75 15.5 12.75C25.6493 12.75 20.5 12 30.5 9C40.5 6 42.8175 10.0709 49 12.75C56.5 16 57 20 66.5 20"
-			stroke="url(#paint0_linear_72:323)"
-			strokeWidth="2"
-			strokeLinejoin="round"
-		/>
+	<svg xmlns="http://www.w3.org/2000/svg" width="68" height="21" viewBox="0 0 68 21" fill="none">
+		<rect opacity="0.7" width="17" height="21" rx="1" fill="#05050F"/>
+		<rect opacity="0.7" x="19" width="14" height="21" rx="1" fill="#05050F"/>
+		<rect opacity="0.7" x="35" width="14" height="21" rx="1" fill="#05050F"/>
+		<rect opacity="0.7" x="51" width="17" height="21" rx="1" fill="#05050F"/>
+		<path d="M0 18C8.62687 18 7.35075 11.5 17.5 11.5C27.6493 11.5 26.8731 17.5 35.5 17.5C44.1269 17.5 46.3404 13.5246 53 12.5C59.5 11.5 58.8731 8.5 67.5 8.5" stroke="#483D8B" stroke-opacity="0.8" stroke-width="2" stroke-linejoin="round"/>
+		<path d="M0 6C8.62687 6 5.35075 12.75 15.5 12.75C25.6493 12.75 20.5 12 30.5 9C40.5 6 42.8175 10.0709 49 12.75C56.5 16 57 20 66.5 20" stroke="url(#paint0_linear_72:323)" stroke-width="2" stroke-linejoin="round"/>
 		<defs>
 			<linearGradient id="paint0_linear_72:323" x1="67" y1="7.96873" x2="1.67368" y2="8.44377" gradientUnits="userSpaceOnUse">
-				<stop stopColor="#FF0000" />
-				<stop offset="1" stopColor="#E4514D" />
+				<stop stop-color="#FF0000"/>
+				<stop offset="1" stop-color="#E4514D"/>
 			</linearGradient>
 		</defs>
 	</svg>
--- assets/images/graphs/YellowGraph.tsx
+++ assets/images/graphs/YellowGraph.tsx
@@ -1,21 +1,17 @@
 import React from 'react';
 
 const YellowGraph = () => (
-	<svg width="68" height="21" viewBox="0 0 68 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-		<rect opacity="0.7" width="17" height="21" rx="1" fill="#05050F" />
-		<rect opacity="0.7" x="19" width="14" height="21" rx="1" fill="#05050F" />
-		<rect opacity="0.7" x="35" width="14" height="21" rx="1" fill="#05050F" />
-		<rect opacity="0.7" x="51" width="17" height="21" rx="1" fill="#05050F" />
-		<path
-			d="M0 6C8.62687 6 6.85075 12.75 17 12.75C27.1493 12.75 25.3731 9 34 9C42.6269 9 42.262 13.875 49 13.875C55.5398 13.875 58.3731 6 67 6"
-			stroke="url(#paint0_linear_72:548)"
-			strokeWidth="2"
-			strokeLinejoin="round"
-		/>
+	<svg xmlns="http://www.w3.org/2000/svg" width="68" height="21" viewBox="0 0 68 21" fill="none">
+		<rect opacity="0.7" width="17" height="21" rx="1" fill="#05050F"/>
+		<rect opacity="0.7" x="19" width="14" height="21" rx="1" fill="#05050F"/>
+		<rect opacity="0.7" x="35" width="14" height="21" rx="1" fill="#05050F"/>
+		<rect opacity="0.7" x="51" width="17" height="21" rx="1" fill="#05050F"/>
+		<path d="M0 18C8.62687 18 7.35075 11.5 17.5 11.5C27.6493 11.5 26.8731 17.5 35.5 17.5C44.1269 17.5 46.3404 13.5246 53 12.5C59.5 11.5 58.8731 8.5 67.5 8.5" stroke="#483D8B" stroke-opacity="0.8" stroke-width="2" stroke-linejoin="round"/>
+		<path d="M0 6C8.62687 6 6.85075 12.75 17 12.75C27.1493 12.75 25.3731 9 34 9C42.6269 9 42.262 13.875 49 13.875C55.5398 13.875 58.3731 6 67 6" stroke="url(#paint0_linear_72:313)" stroke-width="2" stroke-linejoin="round"/>
 		<defs>
-			<linearGradient id="paint0_linear_72:548" x1="67" y1="7.96873" x2="1.67368" y2="8.44377" gradientUnits="userSpaceOnUse">
-				<stop stopColor="#FFD422" />
-				<stop offset="1" stopColor="#FF7D05" />
+			<linearGradient id="paint0_linear_72:313" x1="67" y1="7.96873" x2="1.67368" y2="8.44377" gradientUnits="userSpaceOnUse">
+				<stop stop-color="#FFD422"/>
+				<stop offset="1" stop-color="#FF7D05"/>
 			</linearGradient>
 		</defs>
 	</svg>
